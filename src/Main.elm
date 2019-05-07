bcbc742804571cfbf46df4a6a7f1a5a4f782fbc3
Jon <lazarini.jonathan@gmail.com>
2019-05-07 19:27:39 +0100

    render a dummy static todo

--- src/Main.elm
+++ src/Main.elm
@@ -1,7 +1,27 @@
 module Main exposing (main)
 
-import Html exposing (text)
+import Html exposing (..)
+
+
+type alias Todo =
+    { description : String
+    }
+
+
+type alias Model =
+    { todolist : List Todo
+    }
+
+
+type Msg
+    = NoOp
+
+
+renderTodoList : List Todo -> Html Msg
+renderTodoList todolist =
+    ul []
+        (List.map (\todo -> li [] [ text todo.description ]) todolist)
 
 
 main =
-    text "hello you!!"
+    renderTodoList [ { description = "first todo" } ]
