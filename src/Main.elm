3908cb29f7d7acfd9a26e6bea752f1d4502d9f31
Jon <lazarini.jonathan@gmail.com>
2019-05-08 14:07:02 +0100

    hook lifecycle

--- src/Main.elm
+++ src/Main.elm
@@ -1,6 +1,39 @@
 module Main exposing (main)
 
+import Browser
 import Html exposing (..)
+
+
+main =
+    Browser.element
+        { init = init
+        , update = update
+        , subscriptions = subscriptions
+        , view = view
+        }
+
+
+init : Model -> ( Model, Cmd Msg )
+init model =
+    ( { todolist = [ { description = "description 1" } ] }, Cmd.none )
+
+
+type Msg
+    = NoOp
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        NoOp ->
+            ( model
+            , Cmd.none
+            )
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.none
 
 
 type alias Todo =
@@ -13,15 +46,16 @@ type alias Model =
     }
 
 
-type Msg
-    = NoOp
-
-
 renderTodoList : List Todo -> Html Msg
-renderTodoList todolist =
+renderTodoList list =
     ul []
-        (List.map (\todo -> li [] [ text todo.description ]) todolist)
+        (List.map (\todo -> li [] [ text todo.description ]) list)
 
 
-main =
-    renderTodoList [ { description = "first todo" } ]
+view : Model -> Html Msg
+view model =
+    case model of
+        todoList ->
+            div []
+                [ renderTodoList model.todolist
+                ]
